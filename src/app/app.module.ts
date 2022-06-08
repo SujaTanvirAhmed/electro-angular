412f6c18e52e0ad54625b7d9b15521c1855b1da0
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2022-06-09 00:15:01 +0600

    first commit and added banner and footer section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
